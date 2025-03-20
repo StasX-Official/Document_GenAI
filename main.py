eb57c1b2162316d220c31108a0db8b205e193fa0
Kozosvyst Stas (StasX) <141828909+StasX-Official@users.noreply.github.com>
2025-03-20 10:26:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,30 @@
 import time, sys
+from google import genai
 
 class app:
   def __init__(self):
     self.API=""
 
+  def document_gen(self, data)
+     pass
+  
   def generate(self,promt):
-    
-    return True
-
+    client = genai.Client(api_key=self.API)
+    response = client.models.generate_content(
+       model="gemini-2.0-flash", contents=str(prompt)
+    )
+    app.document_gen(self, response.text)
+  
   def menu(self):
     print(
     x=input("Promt: ")
+    if x!="exit":
+      app.generate(self, x)
+    else:
+      sys.exit()
 
-AI=app()
 try:
+  AI=app()
   AI.menu()
 finally:
   time.sleep(3)
