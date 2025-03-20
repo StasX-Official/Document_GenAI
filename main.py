b6b70232dac3ddf37cca708c341023825bcc700e
Kozosvyst Stas (StasX) <141828909+StasX-Official@users.noreply.github.com>
2025-03-20 10:22:17 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+import time, sys
+
+class app:
+  def __init__(self):
+    self.API=""
+
+  def generate(self,promt):
+    
+    return True
+
+  def menu(self):
+    print(
+    x=input("Promt: ")
+
+AI=app()
+try:
+  AI.menu()
+finally:
+  time.sleep(3)
+  sys.exit()
